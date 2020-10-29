f63a291e6dd0bfe87682075801c6ad76e85afd92
Arno <r0741327@student.thomasmore.be>
2020-10-29 20:06:27 +0100

    Created ReactionController with GetReactions

--- AngularProjectAPI/Models/Reaction.cs
+++ AngularProjectAPI/Models/Reaction.cs
@@ -7,5 +7,15 @@ namespace AngularProjectAPI.Models
 {
     public class Reaction
     {
+        public int ReactionID { get; set; }
+        public int UserID { get; set; }
+        public int ArticleID { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+
+        //Relations
+        public User User { get; set; }
+        public Article Article { get; set; }
+
     }
 }
