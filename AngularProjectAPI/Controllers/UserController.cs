014bef7d79b8ea18e04ca3bd0bd76151ee946dc9
Arno <r0741327@student.thomasmore.be>
2020-11-13 14:32:56 +0100

    Update for user

--- AngularProjectAPI/Controllers/UserController.cs
+++ AngularProjectAPI/Controllers/UserController.cs
@@ -56,6 +56,38 @@ namespace AngularProjectAPI.Controllers
             return Ok(user);
         }
 
+        // PUT: api/User/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.UserID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
         [HttpDelete("{id}")]
         public async Task<ActionResult<User>> DeleteUser(int id)
         {
@@ -70,5 +102,12 @@ namespace AngularProjectAPI.Controllers
 
             return user;
         }
+
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.UserID == id);
+        }
+
     }
 }
